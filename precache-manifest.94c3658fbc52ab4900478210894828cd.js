110aaad9426ad9524fa2f99ea58380a70035c87b
Nacho Caiafa <nachokai@outlook.com>
2020-07-09 01:17:17 -0300

    Updates

---
+++ precache-manifest.94c3658fbc52ab4900478210894828cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d5fc8faa77d1acfc9142ae3514324e",
+    "url": "/react-likes-app/index.html"
+  },
+  {
+    "revision": "8c06acc69d729711fef1",
+    "url": "/react-likes-app/static/css/2.8c08f09a.chunk.css"
+  },
+  {
+    "revision": "193e5839cf8ecb577225",
+    "url": "/react-likes-app/static/css/main.9513faf1.chunk.css"
+  },
+  {
+    "revision": "8c06acc69d729711fef1",
+    "url": "/react-likes-app/static/js/2.c2957524.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/react-likes-app/static/js/2.c2957524.chunk.js.LICENSE"
+  },
+  {
+    "revision": "193e5839cf8ecb577225",
+    "url": "/react-likes-app/static/js/main.38d2a70d.chunk.js"
+  },
+  {
+    "revision": "8a96c726bd9214900645",
+    "url": "/react-likes-app/static/js/runtime-main.9edffa98.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-likes-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-likes-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-likes-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-likes-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-likes-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
